c613acff0e1fb211940ef189f217f9973df533d2
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-07 13:55:47 -0500

    exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,28 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create 3 more stores
+Store.create(name: 'Surrey', 
+annual_revenue: 224000, 
+mens_apparel: false, 
+womens_apparel: true)
+
+Store.create(name: 'Whistler',
+annual_revenue: 1900000,
+mens_apparel: true,
+womens_apparel: false)     
+
+Store.create(name: 'Yaletown',
+annual_revenue: 430000,
+mens_apparel: true,
+womens_apparel: true)
+
+# Find all mens stores
+@mens_stores = Store.where(mens_apparel: true)
+
+# Loop through the array of mens stores and print each one's name and revenue
+@mens_stores.each { |store| puts store.name, store.annual_revenue }
+
+# Find all womens stores generating less than 1 M annual revenue
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
