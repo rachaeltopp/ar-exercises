e8e61b964f372a7ab6b8550018b36aae69c5c6f8
Rachael Topp <topp@ualberta.ca>
2017-11-07 18:31:28 +0000

    completed exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first to stores to instance variables
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Update the first store in the database
+@store1.name = 'Rachaels'
